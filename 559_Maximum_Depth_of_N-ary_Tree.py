ba8b5984823290afa5dbd0b93daed7f2e587cf5b
AnnaDai1001 <qingqingdai@yahoo.com>
2021-04-18 00:18:58 -0400

    Create 559_Maximum_Depth_of_N-ary_Tree.py

---
+++ 559_Maximum_Depth_of_N-ary_Tree.py
@@ -0,0 +1,22 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    def maxDepth(self, root: 'Node') -> int:
+        # BFS
+        queue = []
+        if root: queue.append((root, 1))
+        depth = 0
+        for (node, level) in queue:
+            depth = level
+            queue += [(child, level+1) for child in node.children]
+        return depth
+            
+        
+        # if not root: return 0
+        # return 1 + max(map(self.maxDepth, root.children or [None]))
