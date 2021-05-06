43186a8054a633097c14bb8d0211675ce00b8a50
Ching-Ching Dai <qingqingdai@yahoo.com>
2021-05-06 15:59:27 -0400

    Update 643_Maximum Average Subarray I.py

--- 643_Maximum Average Subarray I.py
+++ 643_Maximum Average Subarray I.py
@@ -1,16 +1,27 @@
 class Solution:
     def findMaxAverage(self, nums: List[int], k: int) -> float:
+        store_sum = sum(nums[:k])
+        max_sum = store_sum
         
-        i = 1
-        stop = len(nums) - k
-        temp_sum = sum(nums[:k])
-        max_sum = temp_sum
-        while i <= stop:
-            temp_sum = temp_sum - nums[i-1] + nums[i+k-1]
-            if temp_sum > max_sum:
-                max_sum = temp_sum
-            i = i + 1
+        # M1
+        for i in range(1,len(nums)-k+1):
+            store_sum = store_sum - nums[i-1] + nums[i+k-1]
+            if store_sum > max_sum:
+                max_sum = store_sum
+        
+        # M2 - Time Limit Exceeded
+        # for i in range(1, len(nums)-k+1):
+        #     store_sum = sum(nums[i:(i+k)])
+        #     if store_sum > max_sum:
+        #         max_sum = store_sum
+        
+        # M3
+        # i = 1
+        # while i <= (len(nums) - k):
+        #     store_sum = store_sum - nums[i-1] + nums[i+k-1]
+        #     if store_sum > max_sum:
+        #         max_sum = store_sum
+        #     i += 1
+                   
         return max_sum/k
-            
-                
             
