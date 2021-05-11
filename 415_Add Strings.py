87be33170aa02b584a829a904ddd82eeda35463e
Ching-Ching Dai <qingqingdai@yahoo.com>
2021-05-11 17:39:54 -0400

    Update 415_Add Strings.py

--- 415_Add Strings.py
+++ 415_Add Strings.py
@@ -1,3 +1,21 @@
 class Solution:
     def addStrings(self, num1: str, num2: str) -> str:
-        return str(int(num1)+int(num2))
+        def str2int(strs):
+            dic = {'1':1,
+                  '2': 2,
+                  '3': 3,
+                   '4': 4,
+                   '5': 5,
+                   '6': 6,
+                   '7': 7,
+                   '8': 8,
+                   '9': 9,
+                   '0': 0               
+                  }
+            res = 0
+            for s in strs:
+                res = res * 10 + dic[s]
+            return res
+        return str(str2int(num1)+str2int(num2))
+        
+        # return str(int(num1)+int(num2))
