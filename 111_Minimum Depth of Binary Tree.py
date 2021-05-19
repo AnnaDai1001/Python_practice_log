52fd1500f5135696188485cd3b50de20cb39f6c2
Ching-Ching Dai <qingqingdai@yahoo.com>
2021-05-19 19:49:07 -0400

    Update 111_Minimum Depth of Binary Tree.py

--- 111_Minimum Depth of Binary Tree.py
+++ 111_Minimum Depth of Binary Tree.py
@@ -7,13 +7,28 @@
 class Solution:
     def minDepth(self, root: TreeNode) -> int:
         # DFS
-        if not root:
-            return 0
-        if not root.left and not root.right:
-            return 1
-        elif not root.left:
-            return self.minDepth(root.right) + 1
-        elif not root.right:
-            return self.minDepth(root.left) + 1
-        else:
-            return min(map(self.minDepth, (root.left, root.right))) + 1
+        # if not root:
+        #     return 0
+        # if not root.left and not root.right:
+        #     return 1
+        # elif not root.left:
+        #     return self.minDepth(root.right) + 1
+        # elif not root.right:
+        #     return self.minDepth(root.left) + 1
+        # else:
+        #     return min(map(self.minDepth, (root.left, root.right))) + 1
+        
+        # BFS
+        queue = [(root, 1)]
+        
+        while queue:
+            next_visit, curr_len = queue.pop(0)
+            if not next_visit:
+                continue
+            if not next_visit.left and not next_visit.right:
+                return curr_len
+            
+            queue.append((next_visit.left, curr_len+1))
+            queue.append((next_visit.right, curr_len+1))
+        
+        return 0
