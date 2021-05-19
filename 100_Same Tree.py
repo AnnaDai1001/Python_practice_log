0d9ab2951b688c9979840923fdf484928fd38989
Ching-Ching Dai <qingqingdai@yahoo.com>
2021-05-19 19:19:59 -0400

    Create 100_Same Tree.py

---
+++ 100_Same Tree.py
@@ -0,0 +1,44 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSameTree(self, p: TreeNode, q: TreeNode) -> bool:
+        # recursion
+        # if p and q:
+        #     return p.val==q.val and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+        # else:
+        #     return p == q
+        
+        # # DFS with stack
+        # stack = [(p,q)]
+        # while stack:
+        #     node1, node2 = stack.pop()
+        #     if not node1 and not node2:
+        #         continue
+        #     elif None in [node1, node2]:
+        #         return False
+        #     else:
+        #         if node1.val != node2.val:
+        #             return False
+        #         stack.append((node1.left, node2.left))
+        #         stack.append((node1.right, node2.right))
+        # return True
+    
+        # BFS with queue
+        queue = [(p,q)]
+        while queue:
+            node1, node2 = queue.pop(0)
+            if not node1 and not node2:
+                continue
+            elif None in [node1, node2]:
+                return False
+            else:
+                if node1.val != node2.val:
+                    return False
+                queue.append((node1.left, node2.left))
+                queue.append((node1.right, node2.right))
+        return True
+        
