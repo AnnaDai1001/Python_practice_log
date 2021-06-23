01a9f3c65d4c987bb004358cae243424acf72728
Ching-Ching Dai <qingqingdai@yahoo.com>
2021-06-23 00:29:12 -0400

    Create 418_Sentence Screen Fitting.py

---
+++ 418_Sentence Screen Fitting.py
@@ -0,0 +1,5 @@
+# Given a rows x cols screen and a sentence represented by a list of words, find how many times the given sentence can be fitted on the screen.
+# http://bookshadow.com/weblog/2016/10/09/leetcode-sentence-screen-fitting/
+# https://github.com/YaokaiYang-assaultmaster/LeetCode/blob/master/LeetcodeAlgorithmQuestions/418.%20Sentence%20Screen%20Fitting.md
+
+
