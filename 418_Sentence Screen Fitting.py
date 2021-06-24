5a5d21a3d8c71139b3b979e658180f98138d542c
Ching-Ching Dai <qingqingdai@yahoo.com>
2021-06-23 20:04:31 -0400

    Update 418_Sentence Screen Fitting.py

--- 418_Sentence Screen Fitting.py
+++ 418_Sentence Screen Fitting.py
@@ -2,4 +2,21 @@
 # http://bookshadow.com/weblog/2016/10/09/leetcode-sentence-screen-fitting/
 # https://github.com/YaokaiYang-assaultmaster/LeetCode/blob/master/LeetcodeAlgorithmQuestions/418.%20Sentence%20Screen%20Fitting.md
 
+# https://www.youtube.com/watch?v=e987rKv1d7E
 
+class Solution:
+  def wordsTyping(self, sentence, rows, cols):
+    s = ' '.join(sentence) + ' '
+    n = len(s)
+    total_len = 0 # effective length: contain the chars themselves and the space after each word
+    
+    for r in range(rows):
+      total_len += cols
+      if s[total_len%n] == ' ':
+        total_len += 1
+      else:
+        while s[(total_len-1)%n] != ' ' and total_len > 0:
+          total_len -= 1
+    
+    return total_len // n
+      
